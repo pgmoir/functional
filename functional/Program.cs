9aff0cec903e725da50e04831a8774b4e6aa5ffc
Phil Moir <pgmoir@gmail.com>
2017-02-24 13:14:31 +0000

    First build without functional programming revisions - based on pluralsight video training

---
+++ functional/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functional
+{
+    class Program
+    {
+        private static string BuildSelectBox(IDictionary<int, string> options, string id, bool includeUnknown)
+        {
+            var html = new StringBuilder();
+            html.AppendFormat("<select id=\"{0}\" name=\"{0}\">", id);
+            html.AppendLine();
+
+            if (includeUnknown)
+            {
+                html.AppendLine("\t<option>Unknown</option>");
+            }
+
+            foreach (var opt in options)
+            {
+                html.AppendFormat("\t<option value=\"{0}\">{1}</option>", opt.Key, opt.Value);
+                html.AppendLine();
+            }
+
+            html.AppendLine("</select>");
+
+            return html.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            var selections = new Dictionary<int, string>
+            {
+                {1, "Joe Bloggs"},
+                {2, "Jess Smith"},
+                {3, "Cary Grant"},
+                {4, "Jason Bourne"},
+                {5, "Bilbo Baggins"},
+                {6, "Hugh McCarthy"},
+                {7, "Joe Giggins"},
+                {8, "Wesley Snipes"},
+                {9, "Denzel Washington"},
+                {10, "Sam Adams"}
+            };
+
+
+            Console.Write(BuildSelectBox(selections, "names", true));
+            Console.ReadKey();
+        }
+    }
+}
