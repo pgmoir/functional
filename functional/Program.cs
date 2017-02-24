153f065c41070845ff388bd9137c50c90a06ff23
Phil Moir <pgmoir@gmail.com>
2017-02-24 14:15:01 +0000

    Refactored stringbuilder processing statements into functional declarations

--- functional/Program.cs
+++ functional/Program.cs
@@ -2,35 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace functional
 {
+    public static class StringBuilderExtensions
+    {
+        //public static StringBuilder AppendFormattedLine(
+        //    this StringBuilder @this, 
+        //    string format, 
+        //    params object[] args) => @this.AppendFormat(format, args).AppendLine();
+
+        public static StringBuilder AppendFormattedLine(
+            this StringBuilder @this,
+            string format,
+            params object[] args)
+        {
+            return @this.AppendFormat(format, args).AppendLine();
+        }
+
+        public static StringBuilder AppendLineWhen(
+            this StringBuilder @this,
+            Func<bool> predicate,
+            Func<StringBuilder, StringBuilder> fn)
+        {
+            return predicate() ? fn(@this) : @this;
+        }
+
+        public static StringBuilder AppendSequence<T>(
+            this StringBuilder @this,
+            IEnumerable<T> seq,
+            Func<StringBuilder, T, StringBuilder> fn)
+        {
+            return seq.Aggregate(@this, fn);
+        }
+    }
+
     class Program
     {
         private static string BuildSelectBox(IDictionary<int, string> options, string id, bool includeUnknown)
         {
-            var html = new StringBuilder();
-            html.AppendFormat("<select id=\"{0}\" name=\"{0}\">", id);
-            html.AppendLine();
-
-            if (includeUnknown)
-            {
-                html.AppendLine("\t<option>Unknown</option>");
-            }
-
-            foreach (var opt in options)
-            {
-                html.AppendFormat("\t<option value=\"{0}\">{1}</option>", opt.Key, opt.Value);
-                html.AppendLine();
-            }
-
-            html.AppendLine("</select>");
-
-            return html.ToString();
+            return new StringBuilder()
+                .AppendFormattedLine("<select id=\"{0}\" name=\"{0}\">", id)
+                .AppendLineWhen(() => includeUnknown, sb => sb.AppendLine("\t<option>Unknown</option>"))
+                .AppendSequence(options, (sb, opt) => sb.AppendFormattedLine("\t<option value=\"{0}\">{1}</option>", opt.Key, opt.Value))
+                .AppendLine("</select>")
+                .ToString();
         }
 
-        static void Main(string[] args)
+        static void Main()
         {
             var selections = new Dictionary<int, string>
             {
